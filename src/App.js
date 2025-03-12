f105b12b2d287e07f8c2de8c1f747660f2ded473
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-12 19:49:08 +0300

    added all

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import FormComponent from './components/formComponent';
 import LogInComponent from './components/LogInComponent';
+import { useState } from 'react';
 
 function App() {
-  const handleForm = (e) => {}
+  const [formState, setFormState] = useState("register");
+    const handleForm = (e) => {
+    const btnName = e.target.value
+    setFormState(btnName)
+    console.log(formState)
+  }
   return (
     <div>
-      <button id="loginButton" onClick={handleForm} >LOGIN</button>
-      <button id="registerButton" onClick={handleForm}>REGISTER</button>
-      <FormComponent/>
-      <LogInComponent/>
+      <button value="register"  onClick={handleForm}>REGISTER</button>
+      <button onClick={handleForm} >LOGIN</button>
+      { formState === "register" ? <FormComponent/> : <LogInComponent/> }
+      
+                  
     </div>
   );
 }
