53fd698878d3e049d152fa9d38ca95327fe2671c
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-12 21:12:56 +0300

    adding label

--- src/components/LogInComponent.js
+++ src/components/LogInComponent.js
@@ -3,9 +3,9 @@ const LogInComponent = () => {
     return(
         <div>
             <h1>LOGIN</h1>
-            <label for="Email"></label>
+            <label for="Email">Email</label>
             <input id="email" name="email" type="text" placeholder="Email" required />
-            <label for="Password"></label>
+            <label for="Password">Password</label>
             <input id="password" name="Password" type="password" placeholder="Password" required />
             <button id="submitButton">LOGIN</button>
         </div>
