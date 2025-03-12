78c20ec0fe522afb90d6976b333696188744f1d7
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-12 21:12:53 +0300

    adding label

--- src/components/formComponent.js
+++ src/components/formComponent.js
@@ -2,17 +2,17 @@ const FormComponent = () => {
 return(
     <div>
         <h1>REGISTRATION</h1>
-        <label for="FirstName"></label>
+        <label for="FirstName">First Name</label>
         <input id="firstName" name="firstName" type="text" placeholder="First Name" required maxlength="25"/>
-        <label for="LastName"></label>
+        <label for="LastName">Last Name</label>
         <input id="lastName" name="lastName" type="text" placeholder="Last Name" required maxlength="25"/>
-        <label for="Email"></label>
+        <label for="Email">Email</label>
         <input id="email" name="email" type="text" placeholder="Email" required />
-        <label for="PhoneNumber"></label>
+        <label for="PhoneNumber">Phone Number</label>
         <input id="phoneNumber" name="PhoneNumber" type="number" placeholder="Phone Number" required />
-        <label for="Password"></label>
+        <label for="Password"> Password</label>
         <input id="password" name="Password" type="password" placeholder="Password" required />
-        <label for="Confirm Password"></label>
+        <label for="Confirm Password">Confirm Password</label>
         <input id="confirmPassword" name="Password" type="password" placeholder="Confirm Password" required />
         <button id="submitButton">SUBMIT</button>
     </div>
