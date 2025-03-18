14cfa0caf0b534f79de945e9ba4e116d90495b46
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-18 20:22:07 +0300

    styled team members hr

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -1,35 +1,56 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
-import profile from "../images/photo_2023-12-03_16-51-43.jpg"
 import "./ProfileComponent.css"
+import landoNorris from "../images/drivers/lando-norris.avif"
+import unitedkingdom from  "../images/flags/united-kindgom.avif"
 
 
 const ProfileComponent = () => {
     const [team] = useState ([
         {
-            name: "Vansh Patel",
-            role: "Full-Stack Engineering Student",
-            img_url: "/src/images/photo_2023-12-03_16-51-43.jpg",
-            linked_in_url: "https://www.linkedin.com/in/vansh-patel-72650b26a",
-            github_url: "https://github.com/patel-vansh1607",
-            description: "I am Vansh Patel"
+            name: "LANDO NORRIS",
+            origin: unitedkingdom ,
+            racing_team: "Mclaren",
+            img_url: landoNorris,
+            race_number: "4",
+        },
+        {
+            name: "LANDO NORRIS",
+            origin: unitedkingdom ,
+            racing_team: "Mclaren",
+            img_url: landoNorris,
+            race_number: "4",
+        },
+        {
+            name: "GEORGE RUSSELL",
+            origin: unitedkingdom ,
+            racing_team: "Mclaren",
+            img_url: landoNorris,
+            race_number: "4",
         },
         
     ])
 
     return(
         <div className="div2">
-            <h1>Meet the Team</h1>
+            <h1>Meet the Racers</h1>
             <div className="header2">
             {team.map((member, index) =>{
                 return(
-                    <div key={index} className="team-members">
-                        <img src={profile} width={100}></img>
-                        <h2 className="member-name">{member.name}</h2>
-                        <p className="member-role">{member.role}</p>
-                        <p className="member-description">{member.description}</p>
-                        <Link to = {member.github_url}> Github</Link>
-                        <Link to = {member.linked_in_url}> Linked-In</Link>
+                        <div key={index} className="team-members" >
+                            <hr my-xxs border-greyLight></hr>
+                        <div className="intro">
+                            <h2 className="team-name">{member.name}</h2>
+                            <div className="img1">
+                                <img src={member.origin} width={100}></img>
+                            </div>
+                        </div>
+                        <hr my-xxs border-greyLight></hr>
+                        <p className="team-racing_team">{member.racing_team}</p>
+                        <div className="d-img">
+                            <p className="member-racing_number">{member.race_number}</p>
+                            <img src={member.img_url} alt="Driver" />
+                        </div>
+
                     </div>
                 )
             })}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import FooterComp from "../components/footer"
 import HeaderComponent from "../components/HeaderComponent"
+import Navbar from "../components/navBar"
 import ProfileComponent from "../components/ProfileComponent"
 
 const AboutPage = () => {
     return(
         <div>
             <HeaderComponent />
+            <Navbar />
             <ProfileComponent />
             <FooterComp/>
         </div>
