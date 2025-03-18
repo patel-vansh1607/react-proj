711668e2f9754d4fc4d6b2f960743ac1a0fe1a77
Vansh Patel <patel.vansh1607@gmail.com>
2025-03-18 20:38:09 +0300

    imported all drivers

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -1,10 +1,32 @@
 import { useState } from "react"
 import "./ProfileComponent.css"
 import landoNorris from "../images/drivers/lando-norris.avif"
-import unitedkingdom from  "../images/flags/united-kindgom.avif"
 import maxVerstapen from "../images/drivers/max-verstappen.avif"
-import netherlands from  "../images/flags/netherlands.avif"
 import georgeRussell from "../images/drivers/george-russell.avif"
+import kimiAntonelli from "../images/drivers/kimi-antonelli.avif"
+import alexanderAlbon from "../images/drivers/kimi-antonelli.avif"
+import lanceStroll from "../images/drivers/lando-norris.avif"
+import nicoHulkenburg from "../images/drivers/max-verstappen.avif"
+import charlesLeclerc from "../images/drivers/george-russell.avif"
+import oscarPiastri from "../images/drivers/kimi-antonelli.avif"
+import lewisHamilton from "../images/drivers/kimi-antonelli.avif"
+import pierreGasly from "../images/drivers/lando-norris.avif"
+import yukiTsunoda from "../images/drivers/max-verstappen.avif"
+import estabenOcon from "../images/drivers/george-russell.avif"
+import oliverBearman from "../images/drivers/kimi-antonelli.avif"
+import jackDoohan from "../images/drivers/kimi-antonelli.avif"
+import fernandoAlonso from "../images/drivers/lando-norris.avif"
+import gabrielBortoleto from "../images/drivers/max-verstappen.avif"
+import isackHadjar from "../images/drivers/george-russell.avif"
+import laimLawson from "../images/drivers/kimi-antonelli.avif"
+import carlozSainz from "../images/drivers/kimi-antonelli.avif"
+
+
+
+import unitedkingdom from  "../images/flags/united-kindgom.avif"
+
+import netherlands from  "../images/flags/netherlands.avif"
+
 
 const ProfileComponent = () => {
     const [team] = useState ([
